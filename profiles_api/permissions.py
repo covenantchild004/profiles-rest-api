436a160b09abbd2e9419c4a01142d4b42bb0bdc2
Olusegun Adejorin <saadejorin@gmail.com>
2021-03-22 16:26:12 +0200

    Added user permission

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class UpdateownProfile(permissions.BasePermission):
+class UpdateOwnProfile(permissions.BasePermission):
     """Allow user to update their own profile"""
 
     def has_object_permission(self, request, view, obj):
@@ -10,3 +10,13 @@ class UpdateownProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """"Allow users to update their own status"""
+    def has_object_permission(self, request, view, obj):
+        """Check the iser is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
+
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ertrugulghazi/Courses/profiles-rest-api/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
